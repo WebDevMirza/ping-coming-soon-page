5f6fd41625a4a011e6a7b2a2cbf90cb8790404dc
WebDev Mirza <webdevmirza@gmail.com>
2023-01-27 19:36:50 +0600

    The way to deploy on github

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,10 @@ export default defineConfig({
   base: "/ping-coming-soon-page/",
   plugins: [react()],
 });
+
+//  1. add `base` with homepage url
+//  2. add to package.json
+//            "predeploy": "npm run build",
+//            "deploy": "gh-pages -d dist",
+
+// 3. npm install gh-pages --save-dev
